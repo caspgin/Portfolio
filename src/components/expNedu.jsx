2703e7d8e0247b6c0f1b7c24a16930ee415b4c5a
caspgin <31950557+caspgin@users.noreply.github.com>
2025-09-17 16:48:55 -0700

    Updating components

---
+++ src/components/expNedu.jsx
@@ -0,0 +1,72 @@
+export const Exp = () => {
+	return (
+		<div className="w-full ml-10 p-2">
+			<div className=" ml-10  rounded-lg flex flex-col gap-1 connector ">
+				<time className="text-xs text-gray-400">
+					April 2021 - August 2022
+				</time>
+				<h2 className="font-semibold text-lg leading-none">PopReach</h2>
+				<p className="text-[0.9rem] text-gray-400">
+					Pipeline And Graphics Developer
+				</p>
+				<div className=" pl-3 text-sm text-gray-400">
+					<ul className="list-disc flex flex-col gap-1">
+						<li>
+							Part of PayDay Crime War mobile FPS game dev team.
+						</li>
+						<li>
+							Build PBS shaders in HLSL/Cg for weapons and
+							characters in the Built-in render pipeline of
+							unity{' '}
+						</li>
+						<li>
+							Build pipeline to get 3d models and textural assets
+							from S3 -{'>'} Maya/Photshop -{'>'} Unity and into
+							the game.
+						</li>
+						<li>
+							Develop tools in maya, photoshop and unity to speed
+							up rigourous asset clean up process and to help lead
+							artist.{' '}
+						</li>
+					</ul>
+				</div>
+				<div
+					className="flex gap-1 flex-wrap mt-2 text-neutral-200 text-sm
+					*:border-1
+					*:border-white
+					*:rounded-full
+					*:p-1
+					*:px-2
+					*:font-semibold"
+				>
+					<span>Unity</span>
+					<span>Maya</span>
+					<span>Photoshop</span>
+					<span>Javascript</span>
+					<span>Python</span>
+					<span>C#</span>
+					<span>HLSL/Cg</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export const Edu = () => {
+	return (
+		<div className="w-full ml-10 p-2 py-4">
+			<div className=" ml-10  rounded-lg flex flex-col gap-1 connector ">
+				<time className="text-xs text-gray-400">
+					September 2021 - December 2023
+				</time>
+				<h2 className="font-semibold text-lg leading-none">
+					Douglas College
+				</h2>
+				<p className="text-[0.9rem] text-gray-400">
+					Computer Science Diploma
+				</p>
+			</div>
+		</div>
+	);
+};
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,35 +1,44 @@
 import { GithubSVG, Link, LinkedInSVG, ResumeSVG } from '../components';
 import profile from '/Profile_3_2698.png';
 
-
 export const Landing = () => {
 	const imageSizes = [300, 800, 1200];
 
 	const generateSrcset = () => {
 		return imageSizes
-			.map((size) => `${profile.replace(/\_2698.png$/, `_${size}.png`)} ${size}w`)
+			.map(
+				(size) =>
+					`${profile.replace(/_2698.png$/, `_${size}.png`)} ${size}w`,
+			)
 			.join(', ');
 	};
 
 	const srcSet = generateSrcset();
 
 	return (
-		<section id="landing" className="py-48 px-3">
+		<section id="landing" className="py-44 px-3">
 			<main className="max-w-l ">
 				<div className="pt-2 flex text-center items-center justify-center gap-4">
 					<div className="relative inline-block w-16 h-16  rounded-full border-1 border-white overflow-clip">
-						<img src={profile} className="absolute h-full w-full inset-0 " sizes='100vw' srcSet={srcSet} alt="Abidali Sarangwala" loading="lazy" decoding="async" />
-
+						<img
+							src={profile}
+							className="absolute h-full w-full inset-0 "
+							sizes="100vw"
+							srcSet={srcSet}
+							alt="Abidali Sarangwala"
+							loading="lazy"
+							decoding="async"
+						/>
 					</div>
 					<h1 className="text-gray-50 text-xl text-left leading-5">
-
 						<span>Abidali</span>
 						<br />
 						<span>Sarangwala</span>
 					</h1>
 				</div>
 				<div className="flex items-center justify-center gap-4">
-					<h1 className="text-5xl text-gray-50 font-semibold text-accenty ">FULLSTACK
+					<h1 className="text-5xl text-gray-50 font-semibold text-accenty ">
+						FULLSTACK
 					</h1>
 				</div>
 				<div className="flex justify-between items-center text-center text-[10px] ">
@@ -38,20 +47,26 @@ export const Landing = () => {
 					</h1>
 					<div className="relative">
 						<div className="relative ml-1 ">
-							<Link href="mailto:abi.sarangwala@gmail.com" linkSymbol={"forward_to_inbox"} linkTitle={"Contact me"} />
+							<Link
+								href="mailto:abi.sarangwala@gmail.com"
+								linkSymbol={'forward_to_inbox'}
+								linkTitle={'Contact me'}
+							/>
 						</div>
 					</div>
 				</div>
-				<div className='pt-2 text-center'>
-					<div className='flex justify-evenly'>
+				<div className="pt-2 text-center">
+					<div className="flex justify-evenly">
 						<Link href="" svg={<LinkedInSVG />} />
 						<Link href="" svg={<GithubSVG />} />
-						<Link href="" svg={<ResumeSVG />} linkTitle={"Resume"} />
+						<Link
+							href=""
+							svg={<ResumeSVG />}
+							linkTitle={'Resume'}
+						/>
 					</div>
-
 				</div>
-			</main >
-		</section >
+			</main>
+		</section>
 	);
-}
-
+};
